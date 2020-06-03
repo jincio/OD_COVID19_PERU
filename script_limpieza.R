f8a71e6312fc7130e807f53b669b9d15c1c151fe
jincio <j.incio@pitt.edu>
2020-06-03 15:30:26 -0400

    pasos script

--- script_limpieza.R
+++ script_limpieza.R
@@ -4,5 +4,12 @@
 #
 #Objetivo: tabla por departamento_dia número de casos positivos
 #
-
-
+# Pasos: 
+# Levantar la data
+#Trabajr las fechas. Todas en el mismo formato
+#Cambiar nombres de columnas: minusculas sin espacio, etc
+#Limpiar nombres de provincia y distrito. 
+#Limpiar otros campos
+#Ubicar duplicados
+#Merge con Ubigeo
+#Calapse table
